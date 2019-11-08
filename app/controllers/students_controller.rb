82519749f43666648c2ef149036d6c5b670a15b9
Doug Kintop <dkintop@hotmail.com>
2019-11-08 15:19:09 -0500

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,33 @@
+class StudentsController < ApplicationController
+
+    def new 
+        @student=Student.new
+    end
+
+    def create 
+        @student = Student.new(student_params(:first_name, :last_name))
+        @student.save
+        redirect_to student_path(@student)
+    end 
+
+    def show 
+
+    end 
+
+    def edit 
+
+    end 
+
+    def update 
+
+    end
+
+    private 
+
+    def student_params(*args)
+        params.require(:student).permit(*args)
+    end
+
+end
+
+ 
